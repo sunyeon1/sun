885c033649d00d6de22edc5d22865260a83fd834
admin <admin@DESKTOP-S98KFBN>
2021-10-12 16:16:19 +0900

    4444

--- hellogti/src/hellogit/helloGit.java
+++ hellogti/src/hellogit/helloGit.java
@@ -4,7 +4,7 @@ public class helloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git1");
+		System.out.println("Hello Git1  ");
 	}
 
 }
